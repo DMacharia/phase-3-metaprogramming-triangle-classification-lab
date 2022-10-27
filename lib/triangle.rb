98eb547a78be31c1d914f04c721a94d7bf9ab41a
Daniel Macharia <danielhuios@gmail.com>
2022-10-27 23:39:17 +0300

    assignment completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require "pry"
 class Triangle
-  # write code here
+    attr_accessor :length, :width, :hypotenuse
+    
+    def initialize(length, width, hypotenuse)
+        @length = length
+        @width = width
+        @hypotenuse = hypotenuse
+    end
+    
+    def kind
+        sum1 = self.length + self.width
+        sum2 = self.length + self.hypotenuse
+        sum3 = self.width + self.hypotenuse
+
+        if self.length <= 0 || self.width <= 0 || self.hypotenuse <= 0 || sum1 <= self.hypotenuse || sum2 <= self.width || sum3 <= self.length
+            raise TriangleError
+        elsif self.length == self.width && self.width == self.hypotenuse && self.length != 0
+            return :equilateral
+        elsif self.length==self.width || self.length==self.hypotenuse || self.width == self.hypotenuse
+            return :isosceles
+        else
+            return :scalene
+        end
+    end
+
+    class TriangleError < StandardError
+        def message
+            "This is not a triangle"
+        end
+    end
 end
+
+ triangle1 = Triangle.new(10, 2, 3)
+ binding.pry
